53fa6352fe691625a1ccccf0dd779b1c731e91eb
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2025-01-08 19:21:08 +0100

    test vercel 34

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+
+module.exports = {
+    // Autres configurations Next.js...
+
+    eslint: {
+        // Permet aux builds de production de se terminer même s'il y a des erreurs ESLint
+        ignoreDuringBuilds: true,
+    },
+};
